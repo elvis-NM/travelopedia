26e7b8bd10f36f4a8f06a5f52f8ff165901d68ad
Elvis Makia <elvis_makia@yahoo.com>
2024-07-13 18:37:59 -0400

    added api provider component

--- src/index.js
+++ src/index.js
@@ -2,13 +2,17 @@ import ReactDOM from "react-dom/client";
 import Header from "./Layout/Header";
 import Footer from "./Layout/Footer";
 import DestinationIndex from "./Components/DestinationIndex";
+import { ApiProvider, Provider } from "@reduxjs/toolkit/dist/query/react";
+import { destinationApi } from "./api/destinationApi";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <div style={{ backgroundColor: "black", color: "grey" }}>
-    <Header />
-    <div className="px-4"></div>
-    <DestinationIndex />
-    <Footer />
+    <ApiProvider api={destinationApi}>
+      <Header />
+      <div className="px-4"></div>
+      <DestinationIndex />
+      <Footer />
+    </ApiProvider>
   </div>
 );
