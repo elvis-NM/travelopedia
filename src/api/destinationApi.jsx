001815cb49e89cdb883098917fe9c1d28573006a
Elvis Makia <elvis_makia@yahoo.com>
2024-07-13 19:16:31 -0400

    fetch data configurations

--- src/api/destinationApi.jsx
+++ src/api/destinationApi.jsx
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const destinationApi = createApi({
   reducerPath: "apidestination",
-  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:5001/" }),
+  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:8000/" }),
   endpoints: (builder) => ({
     //QUERY -> GET
     //MUTATION -> POST/PUT/DELETE
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import ReactDOM from "react-dom/client";
 import Header from "./Layout/Header";
 import Footer from "./Layout/Footer";
 import DestinationIndex from "./Components/DestinationIndex";
-import { ApiProvider, Provider } from "@reduxjs/toolkit/dist/query/react";
+import { ApiProvider } from "@reduxjs/toolkit/dist/query/react";
 import { destinationApi } from "./api/destinationApi";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
