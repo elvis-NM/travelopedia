ddce5148722452eaf0cdf692ddb506c20927672a
Elvis Makia <elvis_makia@yahoo.com>
2024-07-14 19:34:42 -0400

    Integrate RTK Query with Redux Store

--- src/index.js
+++ src/index.js
@@ -2,17 +2,19 @@ import ReactDOM from "react-dom/client";
 import Header from "./Layout/Header";
 import Footer from "./Layout/Footer";
 import DestinationIndex from "./Components/DestinationIndex";
-import { ApiProvider } from "@reduxjs/toolkit/dist/query/react";
-import { destinationApi } from "./api/destinationApi";
+// import { ApiProvider } from "@reduxjs/toolkit/dist/query/react";
+//import { destinationApi } from "./api/destinationApi";
+import { Provider } from "react-redux";
+import { store } from "./Redux/store";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <div style={{ backgroundColor: "black", color: "grey" }}>
-    <ApiProvider api={destinationApi}>
+    <Provider store={store}>
       <Header />
       <div className="px-4"></div>
       <DestinationIndex />
       <Footer />
-    </ApiProvider>
+    </Provider>
   </div>
 );
